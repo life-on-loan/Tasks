c23ed933e67e145cc5e55ff83192df2361fab946
life-on-loan <79449918+life-on-loan@users.noreply.github.com>
2026-07-08 10:37:15 +0500

    Update multithreading/improve-code2/solution.java

--- multithreading/improve-code2/solution.java
+++ multithreading/improve-code2/solution.java
@@ -3,14 +3,10 @@ public class MockClient {
     private final AtomicInteger counter = new AtomicInteger();
 
     public CompletableFuture<String> restCall() {
-        int current;
-        do {
-            current = counter.get();
-            if (current >= N) {
-                return CompletableFuture.failedFuture(new RuntimeException("not ok"));
-            }
-        } while (!counter.compareAndSet(current, current + 1));
-
-        return CompletableFuture.completedFuture("ok");
+        if (counter.incrementAndGet() <= N) {
+            return CompletableFuture.completedFuture("ok");
+        } else {
+            return CompletableFuture.failedFuture(new RuntimeException("not ok"));
+        }
     }
 }
